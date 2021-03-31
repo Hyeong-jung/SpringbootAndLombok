b9b8649344731c932b37cb9c93a841f9d2622fd1
monarch7 <monarch7@naver.com>
2021-03-31 14:48:49 +0900

    -- Properties 파일 file Upload, Download Path 설정

--- SpringbootAndLombok/src/main/java/com/springboot/lombok/property/FileUploadProperties.java
+++ SpringbootAndLombok/src/main/java/com/springboot/lombok/property/FileUploadProperties.java
@@ -1,6 +1,8 @@
 package com.springboot.lombok.property;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+
+// 프라퍼티 파일 file 경로 설정
 @ConfigurationProperties(prefix="file")
 public class FileUploadProperties {
 	
