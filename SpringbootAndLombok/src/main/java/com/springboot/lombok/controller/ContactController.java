76337457ad33432cedffc8c1b854cf7c89080e2c
monarch7@naver.com <monarch7@naver.com>
2021-04-05 20:34:14 +0900

    -- Save Test

--- SpringbootAndLombok/src/main/java/com/springboot/lombok/controller/ContactController.java
+++ SpringbootAndLombok/src/main/java/com/springboot/lombok/controller/ContactController.java
@@ -9,12 +9,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springboot.lombok.model.Book;
 import com.springboot.lombok.model.Company;
 import com.springboot.lombok.model.Contact;
+import com.springboot.lombok.model.ContactType;
 import com.springboot.lombok.service.ContactService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -41,6 +44,18 @@ public class ContactController {
         final List<Contact> contacts = contactService.searchByName(firstName, lastName);
         
         return new ResponseEntity<>(contacts, HttpStatus.OK);
+    }
+    
+    
+    
+    @PostMapping(value = "contact/save")
+    public ResponseEntity<Void> save(@RequestBody final Company company, String firstName, String lastName, ContactType type) {
+        log.info("DB 에 contact 데이터 저장", company.toString());
+        
+        // save(Company company, String firstName, String lastName, ContactType type)
+        contactService.save(company, firstName, lastName, type);
+        
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }    
     
 	
