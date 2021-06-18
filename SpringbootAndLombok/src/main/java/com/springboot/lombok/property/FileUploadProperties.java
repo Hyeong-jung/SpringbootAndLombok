63a3d49f32a83ea8bfe4a1b27d33e02b8739c7f1
monarch7 <monarch7@naver.com>
2021-06-18 10:29:26 +0900

    -- Comment Modify

--- SpringbootAndLombok/src/main/java/com/springboot/lombok/property/FileUploadProperties.java
+++ SpringbootAndLombok/src/main/java/com/springboot/lombok/property/FileUploadProperties.java
@@ -2,7 +2,7 @@ package com.springboot.lombok.property;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 
-// 프라퍼티 파일 file 경로 설정
+// Properties 파일 file 경로 설정
 @ConfigurationProperties(prefix="file")
 public class FileUploadProperties {
 	
