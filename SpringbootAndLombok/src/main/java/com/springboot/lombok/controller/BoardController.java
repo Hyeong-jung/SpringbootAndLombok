2bc63d57d80f3f749dd8c486c3b646973e42b4e3
monarch7 <monarch7@naver.com>
2021-07-12 14:26:06 +0900

    -- Add CSV Download Method

--- SpringbootAndLombok/src/main/java/com/springboot/lombok/controller/BoardController.java
+++ SpringbootAndLombok/src/main/java/com/springboot/lombok/controller/BoardController.java
@@ -7,10 +7,15 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+//import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +23,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.lombok.model.Board;
 import com.springboot.lombok.service.BoardService;
+import com.springboot.lombok.service.BoardCsvService;
 import com.springboot.lombok.util.BoardExcelExporter;
 
 import lombok.extern.slf4j.Slf4j;
@@ -40,6 +47,9 @@ public class BoardController {
 
 	@Autowired
 	private BoardService boardService;
+	
+	@Autowired
+	private BoardCsvService csvService;
 	
 	@GetMapping("/board")
 	public ResponseEntity<Map> getAllBoards(@RequestParam(value = "p_num", required=false) Integer p_num) {
@@ -81,8 +91,8 @@ public class BoardController {
 	}
 	
     @GetMapping(value = "/board/excel")
-    public void exportToExcelContacts(HttpServletResponse response) throws IOException {
-        log.info(" Contact DB 호출");
+    public void exportToExcelBoardList(HttpServletResponse response) throws IOException {
+        log.info(" Board DB 호출");
 
         response.setContentType("application/octet-stream");
         DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
@@ -98,6 +108,22 @@ public class BoardController {
         BoardExcelExporter excelExporter = new BoardExcelExporter(boardListAll);
         
         excelExporter.export(response);
+    }
+    
+    //URL - http://localhost:8000/reactapi/board/csv
+    //note - incoming request to contain the mandatory "Content-disposition" and "Content-Type" headers
+    @GetMapping(value = "/board/csv")
+    public ResponseEntity<InputStreamResource> exportToCsvBoardList(@RequestHeader(name = "Content-disposition") final String fileName, 
+    		@RequestHeader(name = "Content-Type") final String mediaType) {
+
+        log.info("Downloading residents csv");
+        final List<Board> boardListAll = boardService.getAllBoard();
+        final InputStreamResource resource = new InputStreamResource(csvService.load(boardListAll));
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, fileName)
+                .contentType(MediaType.parseMediaType(mediaType))
+                .body(resource);
+    	
     }
 	
 }
