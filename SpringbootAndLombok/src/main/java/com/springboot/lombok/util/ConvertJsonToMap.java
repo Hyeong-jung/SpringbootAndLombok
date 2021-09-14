cb8a9ab6a18c577b6d51a5421ed95f811589ad0d
monarch7 <monarch7@naver.com>
2021-09-14 12:02:52 +0900

    -- ConvertJsonToMap

---
+++ SpringbootAndLombok/src/main/java/com/springboot/lombok/util/ConvertJsonToMap.java
@@ -0,0 +1,64 @@
+package com.springboot.lombok.util;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import javax.json.JsonArray;
+import javax.json.JsonException;
+import javax.json.JsonObject;
+
+
+
+public class ConvertJsonToMap {
+
+	
+    public static Map<String, Object> jsonToMap(JsonObject json) {
+        Map<String, Object> retMap = new HashMap<String, Object>();
+
+        if(json != JsonObject.NULL) {
+            retMap = toMap(json);
+        }
+        return retMap;
+    }
+    
+    public static Map<String, Object> toMap(JsonObject object) throws JsonException {
+        Map<String, Object> map = new HashMap<String, Object>();
+
+        Iterator<String> keysItr = object.keySet().iterator();
+        while(keysItr.hasNext()) {
+            String key = keysItr.next();
+            Object value = object.get(key);
+
+            if(value instanceof JsonArray) {
+                value = toList((JsonArray) value);
+            }
+
+            else if(value instanceof JsonObject) {
+                value = toMap((JsonObject) value);
+            }
+            map.put(key, value);
+        }
+        return map;
+    }
+    
+    public static List<Object> toList(JsonArray array) {
+        List<Object> list = new ArrayList<Object>();
+        for(int i = 0; i < array.size(); i++) {
+            Object value = array.get(i);
+            if(value instanceof JsonArray) {
+                value = toList((JsonArray) value);
+            }
+
+            else if(value instanceof JsonObject) {
+                value = toMap((JsonObject) value);
+            }
+            list.add(value);
+        }
+        return list;
+    }
+	
+}
