b5c80ad055a3a822c5505424e9171a7dba78c8ff
monarch7 <monarch7@naver.com>
2021-09-17 15:11:04 +0900

    -- Model And View Map Test

--- SpringbootAndLombok/src/main/java/com/springboot/lombok/controller/TestController.java
+++ SpringbootAndLombok/src/main/java/com/springboot/lombok/controller/TestController.java
@@ -1,7 +1,9 @@
 package com.springboot.lombok.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -101,5 +103,44 @@ public class TestController {
 		return mav; 
 	}	
 
+
+	@RequestMapping("/testmodelnviewmap") 
+	public ModelAndView testmodelnviewmap() throws Exception{
+		
+		Map<String, String> result1 = new HashMap<String, String>();
+		List<Map<String, String>> result2 = new ArrayList<Map<String, String>>();
+		
+		result1.put("key1", "val1");
+		result1.put("key2", "val2");
+		result1.put("key3", "val3");
+		result1.put("key4", "val4");
+		result1.put("key5", "val5");
+		
+		
+		Map<String, String> temp1 = new HashMap<String, String>();
+		Map<String, String> temp2 = new HashMap<String, String>();
+		Map<String, String> temp3 = new HashMap<String, String>();
+		
+		temp1.put("key1-1", "val1-1");
+		temp1.put("key1-2", "val1-2");
+		temp1.put("key1-3", "val1-3");
+		temp2.put("key2-1", "val2-1");
+		temp2.put("key2-2", "val2-2");
+		temp2.put("key2-3", "val2-3");
+		temp3.put("key3-1", "val3-1");
+		temp3.put("key3-2", "val3-2");
+		temp3.put("key3-3", "val3-3");
+		
+		result2.add(temp1);
+		result2.add(temp2);
+		result2.add(temp3);
+		
+		ModelAndView mav = new ModelAndView("testmodelnviewmap");
+		mav.addObject("result1", result1);
+		mav.addObject("result2", result2);
+
+		return mav; 
+	}
+	
 	
 }
